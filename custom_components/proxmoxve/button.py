a65414a571ff6e5e5981ed2913528210efbdc02b
dougiteixeira <31328123+dougiteixeira@users.noreply.github.com>
2024-01-05 19:56:17 -0300

    Fix import DeviceInfo

--- custom_components/proxmoxve/button.py
+++ custom_components/proxmoxve/button.py
@@ -7,7 +7,7 @@ from typing import Final
 from homeassistant.components.button import ButtonEntity, ButtonEntityDescription
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity import DeviceInfo
+from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
