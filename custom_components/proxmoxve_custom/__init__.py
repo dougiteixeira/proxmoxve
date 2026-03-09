eaeb8f5578a703929ee5ac6ca8d05a0b166125fa
Douglas <31328123+dougiteixeira@users.noreply.github.com>
2026-03-09 01:15:57 -0300

    Test

--- custom_components/proxmoxve_custom/__init__.py
+++ custom_components/proxmoxve_custom/__init__.py
@@ -226,6 +226,7 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> b
 
                 coordinators_disk = []
                 for disk in disks if disks is not None else []:
+                    LOGGER.warning("TEST DISK: %s", disk)
                     if isinstance(disk, dict):
                         coordinator_disk = ProxmoxDiskCoordinator(
                             hass=hass,
